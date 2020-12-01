1c4ffb35f9daadb4cf14f60033ac77c3accc8668
Alex Carver <alexander.carver@gmail.com>
2020-12-01 21:25:02 +0000

    Day1a

--- src/day-1a.erl
+++ src/day-1a.erl
@@ -8,15 +8,15 @@
 %% Via list comprehension %%
 %% ---------------------- %%
 
-find_2020_listcomp_2(Ns)
+find_listcomp_2(Ns, T)
 ->
-    [{A, B} | _] = [{A, B} || {A, B, P} <- [{A, B, A + B} || A <- Ns, B <- Ns], P == 2020],
+    [{A, B} | _] = [{A, B} || {A, B, P} <- [{A, B, A + B} || A <- Ns, B <- Ns], P == T],
     {A, B}
 .
 
-find_2020_listcomp_3(Ns)
+find_listcomp_3(Ns, T)
 ->
-    [{A, B, C} | _] = [{A, B, C} || {A, B, C, P} <- [{A, B, C, A + B + C} || A <- Ns, B <- Ns, C <- Ns], P == 2020],
+    [{A, B, C} | _] = [{A, B, C} || {A, B, C, P} <- [{A, B, C, A + B + C} || A <- Ns, B <- Ns, C <- Ns], P == T],
     {A, B, C}
 .
 
@@ -24,42 +24,42 @@ find_2020_listcomp_3(Ns)
 %% Via recursion %%
 %% ------------- %%
 
-find_2020_recursive_n(InputSet, TargetSum, AnswerLength)
+find_recursive_n(InputSet, TargetSum, AnswerLength)
 ->
-    find_2020_recursive_n(InputSet, TargetSum, AnswerLength, [])
+    find_recursive_n(InputSet, TargetSum, AnswerLength, [])
 .
 
 % We've reached the desired amount of answer numbers, and have reached the target - we have our answer:
-find_2020_recursive_n(_, 0, 0, Answer)
+find_recursive_n(_, 0, 0, Answer)
 ->
     Answer
 ;
 
 % We've reached the desired amount of answer numbers, but still haven't reached the target:
-find_2020_recursive_n(_, TargetSum, 0, _) when TargetSum =/= 0
+find_recursive_n(_, TargetSum, 0, _) when TargetSum =/= 0
 ->
     nope
 ;
 
 % We've already overshot the target, don't go any further:
-find_2020_recursive_n(_, TargetSum, _, _) when TargetSum < 0
+find_recursive_n(_, TargetSum, _, _) when TargetSum < 0
 ->
     nope
 ;
 
 % We've run out of numbers in the set, but still haven't reached the target:
-find_2020_recursive_n([], _, _, _)
+find_recursive_n([], _, _, _)
 ->
     nope
 ;
 
-find_2020_recursive_n([HeadOfSet | RemainderOfSet], TargetSum, AnswerLength, AnswerSoFar)
+find_recursive_n([HeadOfSet | RemainderOfSet], TargetSum, AnswerLength, AnswerSoFar)
 ->
-    case find_2020_recursive_n(RemainderOfSet, TargetSum - HeadOfSet, AnswerLength - 1, [HeadOfSet | AnswerSoFar]) of
+    case find_recursive_n(RemainderOfSet, TargetSum - HeadOfSet, AnswerLength - 1, [HeadOfSet | AnswerSoFar]) of
         nope ->
             % We've reached a dead end in this recursion with this number from the set,
             % tail-recurse to the next number in the set:
-            find_2020_recursive_n(RemainderOfSet, TargetSum, AnswerLength, AnswerSoFar)
+            find_recursive_n(RemainderOfSet, TargetSum, AnswerLength, AnswerSoFar)
         ;Answer ->
             Answer
     end
@@ -99,24 +99,34 @@ print_result(Ns)
 %% Entrypoint %%
 %% ---------- %%
 
-main(["listcomp", "2"])
+main(["listcomp", "2", T])
 ->
     Numbers = get_numbers(),
-    {A, B} = find_2020_listcomp_2(Numbers),
+    {A, B} = find_listcomp_2(Numbers, list_to_integer(T)),
     print_result([A, B])
+;
+
+main(["listcomp", "2"])
+->
+    main(["listcomp", "2", "2020"])
+;
+
+main(["listcomp", "3", T])
+->
+    Numbers = get_numbers(),
+    {A, B, C} = find_listcomp_3(Numbers, list_to_integer(T)),
+    print_result([A, B, C])
 ;
 
 main(["listcomp", "3"])
 ->
-    Numbers = get_numbers(),
-    {A, B, C} = find_2020_listcomp_3(Numbers),
-    print_result([A, B, C])
+    main(["listcomp", "3", "2020"])
 ;
 
 main(["recursive", N, T])
 ->
     Numbers = get_numbers(),
-    Result = find_2020_recursive_n(Numbers, list_to_integer(T), list_to_integer(N)),
+    Result = find_recursive_n(Numbers, list_to_integer(T), list_to_integer(N)),
     print_result(Result)
 ;
 
